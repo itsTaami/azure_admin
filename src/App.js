d09119a133a7ffe3960b7fa8eb52be3a93657cf6
Evening <evening@admins-iMac.local>
2023-03-22 17:47:20 +0800

    Mar-22

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+// routes
+import Router from './routes';
+// theme
+import ThemeProvider from './theme';
+// components
+import { StyledChart } from './components/chart';
+import ScrollToTop from './components/scroll-to-top';
+import AuthProvider from './context/AuthContext';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <AuthProvider>
+           <ScrollToTop />
+           <StyledChart />
+           <Router />
+          </AuthProvider>         
+        </ThemeProvider>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
+}
---
+++ src/sections/auth/login/LoginForm.js
@@ -0,0 +1,67 @@
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+// @mui
+import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+// components
+import Iconify from '../../../components/iconify';
+import { AuthContext } from '../../../context/AuthContext';
+
+// ----------------------------------------------------------------------
+
+export default function LoginForm() {
+  const navigate = useNavigate();
+  const { setUser } = useContext(AuthContext);
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('Baraa@gmail.com');
+  const [password, setPassword] = useState('123');
+
+  const handleClick = async () => {
+    // navigate('/dashboard', { replace: true });
+    try {
+      const result = await axios.post(`http://localhost:8000/users/login`, { email, password });
+      console.log(result);
+      setUser(result.data.user[0]);
+      navigate('/dashboard', { replace: true });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Stack spacing={3}>
+        <TextField name="email" label="Email address" value={email} />
+
+        <TextField
+          name="password"
+          label="Password"
+          value={password}
+          type={showPassword ? 'text' : 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+
+      <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+        <Checkbox name="remember" label="Remember me" />
+        <Link variant="subtitle2" underline="hover">
+          Forgot password?
+        </Link>
+      </Stack>
+
+      <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
+        Login
+      </LoadingButton>
+    </>
+  );
+}
